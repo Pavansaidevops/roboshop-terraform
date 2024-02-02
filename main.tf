2cd7f38d69bf42e85426414d8140700418d7d297
Pavan Sai <pavansaidevops7@gmail.com>
2024-02-02 16:28:38 +0530

    roboshop terraform

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "components" {
-  source                 = "git::https://github.com/Pavansaidevops/tf-module-basic-test.git"
+  source   = "git::https://github.com/Pavansaidevops/tf-module-basic-test.git"
   for_each = var.components
 
   zone_id                = var.zone_id
