3defaf892530a827702850ab367ac30b7f4727d1
Pavan Sai <pavansaidevops7@gmail.com>
2024-02-12 11:03:38 +0530

    terraform vpc and subnets

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "components" {
-  source          = "git::https://github.com/Pavansaidevops/tf-module-basic-test.git"
+  source          = "git::https://github.com/Pavansaidevops/tf-module-vpc.git"
   for_each        = var.vpc
   cidr            = each.value["cidr"]
   subnets         = each.value["subnets"]
