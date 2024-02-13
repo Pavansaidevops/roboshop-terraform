4913ae0203b411e753a14be33745468dd1ea3520
Pavan Sai <pavansaidevops7@gmail.com>
2024-02-13 13:22:36 +0530

    terraform roboshop

--- main.tf
+++ main.tf
@@ -1,9 +1,13 @@
-module "components" {
+module "vpc" {
   source          = "git::https://github.com/Pavansaidevops/tf-module-vpc.git"
   for_each        = var.vpc
   cidr            = each.value["cidr"]
   subnets         = each.value["subnets"]
 
+}
+
+output "vpc" {
+  value  = module.vpc
 }
 
 
