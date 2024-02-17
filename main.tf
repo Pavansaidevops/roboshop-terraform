96ca92b1d287b78d957b71fd81115597cdc05b8a
Pavan Sai <pavansaidevops7@gmail.com>
2024-02-17 11:18:59 +0530

    terraform roboshop docdb

--- main.tf
+++ main.tf
@@ -34,6 +34,7 @@ module "docdb" {
   vpc_id                      = local.vpc_id
   sg_ingress_cidr             = local.app_subnets_cidr
   engine_version              = each.value["engine_version"]
+  engine_family               = each.value["engine_family"]
   tags                        = var.tags
   env                         = var.env
 }
